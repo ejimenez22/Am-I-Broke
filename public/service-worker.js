d08f85510b8a6f281cf4d1eb7f8effbcaaad6998
Enrique Jimenez <ejimenez2292@gmail.com>
2022-06-10 22:08:05 -0500

    started on service-workers.js

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'my-site-cache-v1'
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/public/css/styles.css',
+    '/public/js/index.js',
+    '/public/js/idb.js',
+    '/public/icons/icon-72x72.png',
+    '/public/icons/icon-96x96.png',
+    '/public/icons/icon-128x128.png',
+    '/public/icons/icon-144x144.png',
+    '/public/icons/icon-152x152.png',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-384x384.png',
+    '/public/icons/icon-512x512.png'
+
+];
+
