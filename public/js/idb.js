77a5dde226d0d4c8dd844770c6fac53da2a71225
Enrique Jimenez <ejimenez2292@gmail.com>
2022-06-11 21:51:22 -0500

    added idb.js, testing PWA functionality before pushing to ATLAS

---
+++ public/js/idb.js
@@ -0,0 +1,63 @@
+let db;
+const request = indexedDB.open('budget-tracker', 1)
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result
+    db.createObjectStore('new-transaction', { autoIncrement: true })
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result
+
+    if (navigator.onLine) {
+        uploadTransaction()
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new-transaction'], 'readwrite')
+
+    const transactionObjectStore = transaction.objectStore('new-transaction')
+    
+    transactionObjectStore.add(record)
+}
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new-transaction'], 'readwrite')
+
+    const transactionObjectStore = transaction.objectStore('new-transaction')
+
+    const getAll = transactionObjectStore.getAll()
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+
+                const transaction = db.transaction(['new-transaction'], 'readwrite')
+                const transactionObjectStore = transaction.objectStore('new-transaction')
+                transactionObjectStore.clear()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction)
